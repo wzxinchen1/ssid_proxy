976a2da48590e4728deb8e7eabe822a49e729fcc
wzxinchen <1101385391@qq.com>
2025-06-15 18:36:03 +0800

    11

--- js/template.js
+++ js/template.js
@@ -19,7 +19,7 @@ class TemplateEngine {
     // 解析模板为 DOM 树
     const templateElement = document.createElement("template");
     templateElement.innerHTML = template;
-    const domTree = templateElement.firstElementChild;
+    const domTree = templateElement.content.firstElementChild;
 
     // 记录 v-for 元素
     let vForElements = [];
