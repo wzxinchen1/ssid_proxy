e8ad15f8c1177fd7fd45635f04a3348d5b9cce60
wzxinchen <1101385391@qq.com>
2025-08-04 08:57:17 +0800

    11

---
+++ api/game.lua
@@ -0,0 +1,8 @@
+module("luci.controller.ssid-proxy.api.game", package.seeall)
+local Get = {}
+
+function Get.IP()
+    local http = require "luci.http"
+    local client_ip = http.getenv("REMOTE_ADDR")
+    return client_ip
+end
